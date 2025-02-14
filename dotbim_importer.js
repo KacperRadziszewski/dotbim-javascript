f6a33d56ef40f221ffb24a017e1e7c4af8029fe6
KacperRadziszewski <100866159+KacperRadziszewski@users.noreply.github.com>
2025-02-14 01:13:21 +0100

    dotbim to geojson

--- dotbim_importer.js
+++ dotbim_importer.js
@@ -1,101 +1,57 @@
 /*
-  DotBIM JavaScript Library
-  -------------------------
-  A standalone library to read from and write to .bim files.
-
-  Dependencies: None (pure JavaScript)
-
-  Documentation:
-  - Class and Parameter Descriptions included inline.
-  - parseBim(jsonString): Parses .bim JSON into BimFile.
-  - encodeBim(bimFile): Serializes a BimFile to JSON string.
+  DotBIM JavaScript Library with GeoJSON Export
+  ---------------------------------------------
+  A standalone library to read from, write to, and convert .bim files to GeoJSON.
 */
 
-class Color {
-  constructor(r, g, b, a) {
-    this.r = r; // Red (0-255)
-    this.g = g; // Green (0-255)
-    this.b = b; // Blue (0-255)
-    this.a = a; // Alpha (0-255)
-  }
-}
-
-class Vector {
-  constructor(x, y, z) {
-    this.x = x; // X-axis coordinate
-    this.y = y; // Y-axis coordinate
-    this.z = z; // Z-axis coordinate
-  }
-}
-
-class Rotation {
-  constructor(qx, qy, qz, qw) {
-    this.qx = qx; // Quaternion X
-    this.qy = qy; // Quaternion Y
-    this.qz = qz; // Quaternion Z
-    this.qw = qw; // Quaternion W
-  }
-}
-
-class Mesh {
-  constructor(meshId, coordinates, indices) {
-    this.meshId = meshId; // Mesh unique ID
-    this.coordinates = coordinates; // Vertex coordinates
-    this.indices = indices; // Mesh face indices
-  }
-}
-
-class Info {
-  constructor(data) {
-    Object.assign(this, data); // Metadata key-value pairs
-  }
-}
-
-class Element {
-  constructor(guid, meshId, vector, rotation, type, color, faceColors, info) {
-    this.guid = guid; // Unique identifier (GUID)
-    this.meshId = meshId; // Associated mesh ID
-    this.vector = vector; // Position (Vector)
-    this.rotation = rotation; // Orientation (Rotation)
-    this.type = type; // Element type string
-    this.color = color; // Primary color (Color)
-    this.faceColors = faceColors; // Optional per-face colors
-    this.info = info; // Additional metadata (Info)
-  }
-}
-
-class BimFile {
-  constructor(schemaVersion, info, meshes, elements) {
-    this.schemaVersion = schemaVersion; // BIM schema version
-    this.info = info; // File metadata (Info)
-    this.meshes = meshes; // Array of Mesh objects
-    this.elements = elements; // Array of Element objects
-  }
-}
+class Color { constructor(r, g, b, a) { this.r = r; this.g = g; this.b = b; this.a = a; } }
+class Vector { constructor(x, y, z) { this.x = x; this.y = y; this.z = z; } }
+class Rotation { constructor(qx, qy, qz, qw) { this.qx = qx; this.qy = qy; this.qz = qz; this.qw = qw; } }
+class Mesh { constructor(meshId, coordinates, indices) { this.meshId = meshId; this.coordinates = coordinates; this.indices = indices; } }
+class Info { constructor(data) { Object.assign(this, data); } }
+class Element { constructor(guid, meshId, vector, rotation, type, color, faceColors, info) { this.guid = guid; this.meshId = meshId; this.vector = vector; this.rotation = rotation; this.type = type; this.color = color; this.faceColors = faceColors; this.info = info; } }
+class BimFile { constructor(schemaVersion, info, meshes, elements) { this.schemaVersion = schemaVersion; this.info = info; this.meshes = meshes; this.elements = elements; } }
 
 function parseBim(jsonString) {
-  const data = JSON.parse(jsonString);
-  return new BimFile(
-    data.schema_version,
-    new Info(data.info),
-    data.meshes.map(m => new Mesh(m.mesh_id, m.coordinates, m.indices)),
-    data.elements.map(e =>
-      new Element(
-        e.guid,
-        e.mesh_id,
-        new Vector(e.vector.x, e.vector.y, e.vector.z),
-        new Rotation(e.rotation.qx, e.rotation.qy, e.rotation.qz, e.rotation.qw),
-        e.type,
-        new Color(e.color.r, e.color.g, e.color.b, e.color.a),
-        e.face_colors || null,
-        new Info(e.info)
-      )
-    )
-  );
+    const data = JSON.parse(jsonString);
+    return new BimFile(
+        data.schema_version,
+        new Info(data.info),
+        data.meshes.map(m => new Mesh(m.mesh_id, m.coordinates, m.indices)),
+        data.elements.map(e => new Element(
+            e.guid, e.mesh_id,
+            new Vector(e.vector.x, e.vector.y, e.vector.z),
+            new Rotation(e.rotation.qx, e.rotation.qy, e.rotation.qz, e.rotation.qw),
+            e.type, new Color(e.color.r, e.color.g, e.color.b, e.color.a),
+            e.face_colors || null, new Info(e.info))
+        )
+    );
 }
 
 function encodeBim(bimFile) {
-  return JSON.stringify(bimFile, null, 2); // Serialize BIM file to JSON
+    return JSON.stringify(bimFile, null, 2);
 }
 
-export { Color, Vector, Rotation, Mesh, Info, Element, BimFile, parseBim, encodeBim };
+function convertBimToGeoJSON(bimFile, selectedTypes) {
+    const features = bimFile.elements
+        .filter(element => selectedTypes.includes(element.type))
+        .map(element => ({
+            type: "Feature",
+            properties: {
+                guid: element.guid,
+                type: element.type,
+                info: element.info
+            },
+            geometry: {
+                type: "Point",
+                coordinates: [element.vector.x, element.vector.y, element.vector.z]
+            }
+        }));
+
+    return {
+        type: "FeatureCollection",
+        features
+    };
+}
+
+export { Color, Vector, Rotation, Mesh, Info, Element, BimFile, parseBim, encodeBim, convertBimToGeoJSON };
